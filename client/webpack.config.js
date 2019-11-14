be252e5e492055dfbbdcea312819fadc1a69a22b
Tyler Stanczak <tyler@Tylers-MacBook-Pro.local>
2019-11-14 16:49:21 -0600

    add react plugin for eslint

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,34 +1,35 @@
 const path = require('path');
+
 const SRC_DIR = path.join(__dirname, '/src');
 const DIST_DIR = path.join(__dirname, '/public');
 module.exports = {
   entry: `${SRC_DIR}/index.js`,
   output: {
-  filename: 'bundle.js',
-  path: DIST_DIR
+    filename: 'bundle.js',
+    path: DIST_DIR,
   },
-  module : {
-    rules : [
+  module: {
+    rules: [
       {
         test: /\.(js|mjs|jsx)$/,
         enforce: 'pre',
-        loader: 'eslint-loader'
+        loader: 'eslint-loader',
       },
       {
-        test : /\.js?/,
-        include : SRC_DIR,
-        loader : 'babel-loader'
+        test: /\.js?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
       },
       {
         test: /\.css$/,
         include: SRC_DIR,
-        loader: 'css-loader'
-      }
-    ]
+        loader: 'css-loader',
+      },
+    ],
   },
   devServer: {
     contentBase: DIST_DIR,
     compress: true,
-    port: 9000
-  }
+    port: 9000,
+  },
 };
