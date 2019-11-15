7c7035ba483ee02c48217c98bb7b87bf3c4ff3db
Keenan Johns <keenanjj13@protonmail.com>
2019-11-15 13:41:36 -0600

    Updated webpack config

---
+++ client/src/components/DetailsPage/DetailsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './DetailsPage.css';
+
+export default function DetailsPage() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>Header</div>
+      <div className={styles.page_body}>Body</div>
+    </div>
+  );
+}
