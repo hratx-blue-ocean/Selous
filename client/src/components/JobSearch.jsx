b2e85d5b5d88cc286ecbc597911318e8808efe1c
Rob Peschke <rspesc31@gmail.com>
2019-11-15 11:21:31 -0600

    working paper component in job search

--- client/src/components/JobSearch.jsx
+++ client/src/components/JobSearch.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import AssignmentIcon from '@material-ui/icons/Assignment';
 
-export default function JobSearch() {
-  return(
-    <Paper>
-      <Typography>
-        This is a test
+const useStyles = makeStyles({
+  root: {
+    margin: 10,
+  },
+  logo: {
+    margin: 10,
+  },
+});
+
+export default function PaperSheet() {
+  const classes = useStyles();
+
+  return (
+    <Paper className={classes.root}>
+      <Avatar className={classes.logo}>
+        <AssignmentIcon />
+      </Avatar>
+      <Typography variant="h5" component="search_Position">
+        Senior Software Devlopment Engineer
+      </Typography>
+      <Typography component="search_Company">
+        Whole Foods Market
+      </Typography>
+      <Typography component="search_Location">
+        Austin, TX
       </Typography>
     </Paper>
   );
