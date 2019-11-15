7311f725de5f07eabec0bf09a7c07233c9948652
Juan Galeazzi <jgaleazzi@gmail.com>
2019-11-15 15:28:52 -0600

    changes after lunch merged

--- client/src/components/dashboard/dashcolortile.jsx
+++ client/src/components/dashboard/dashcolortile.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-<<<<<<< HEAD
 const DashColorTile = (props) => (
   <>
     <div className={`${props} dash-step-tile`} />
@@ -9,17 +8,5 @@ const DashColorTile = (props) => (
     </div>
   </>
 );
-=======
-const DashColorTile = () => (
-    <>
-      <div className={`${props.class} dash-step-tile`}>   
-      </div>
-      <div>
-        modal goes here
-      </div>
-    </>
-  );
-}
->>>>>>> 2ee665f5b81cde35f6939c2ebd9834e07e0067be
 
 export default DashColorTile;
