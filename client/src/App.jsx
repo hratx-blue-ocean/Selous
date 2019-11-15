da487e4d9ec5b99b3e4247ee2ec4d0bb17cb0fe7
Devin George <devin.k.george@gmail.com>
2019-11-15 11:09:24 -0600

    Add initial layout for goal tracker with material-ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import fetch from 'node-fetch';
+import Goals from './components/goals.jsx';
 // import './App.css';
 
 export default class App extends Component {
--- client/src/components/goals.jsx
+++ client/src/components/goals.jsx
@@ -1,28 +1,107 @@
 import React from 'react';
-import Goal from './goal.jsx';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
 
-const Goals = props => {
-  render() {
-    return (
-      <div className='goals-container'>
-        <div className='goals-header'>
-          <h3 className='goal-title'>Goals</h3>
-        </div>
-        <hr></hr>
-        <div className='goals-body'>
-          {
-            props.currentGoals.map(currentGoal => {
-              <Goal currentGoal={currentGoal} />
-            })
-          }
-        </div>
-        <button 
-          className='goals-button'
-          // onClick={dispatchEvent()}
-        />
-      </div>
-    );
-  }
-};
+const useStyles = makeStyles(theme => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
 
-export default Goals;
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+export default function Goals() {
+  const classes = useStyles();
+
+  return (
+    <>
+      <CssBaseline />
+      <main>
+        {/* Hero unit */}
+        <div className={classes.heroContent}>
+          <Container maxWidth="sm">
+            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+              Goals layout
+            </Typography>
+            <hr />
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map(card => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                       3 / 5
+                    </Typography>
+                    <Typography>
+                      This is the goal
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      -
+                    </Button>
+                    <Button size="small" color="primary">
+                      +
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      {/* Footer */}
+      <footer className={classes.footer}>
+      <Button
+        color="secondary"
+        variant="contained"
+        size="large"
+        className={classes.button}
+        component="a"
+        href="/premium-themes/onepirate/sign-up/"
+      >
+        +
+      </Button>
+      </footer>
+      {/* End footer */}
+    </>
+  );
+}
