d0e1f3f923a323081c3cdc899ab16d87d1de51ff
Keenan Johns <keenanjj13@protonmail.com>
2019-11-15 17:42:56 -0600

    Working on component css scaffolding before breaking it up
    Co-Authored-By: Joel Armendariz <joelarmendariz@users.noreply.github.com>

--- client/src/components/DetailsPage/DetailsPage.jsx
+++ client/src/components/DetailsPage/DetailsPage.jsx
@@ -8,15 +8,29 @@ const DetailsPage = () => (
   <div className={styles.container}>
     <div className={styles.header}>Header</div>
     <div className={styles.page_body}>
-      <div className={styles.company_notes}>Company Notes</div>
+      <div className={styles.company_notes}>
+        <div className={styles.company_notes_box}>Amazon</div>
+      </div>
       <div className={styles.progress_tabs_wrapper}>
-        <div className={styles.progress_tabs_header}>Progress Tabs Header</div>
+        <div className={styles.progress_tabs_header}>
+          <h1>Your progress with Amazon</h1>
+        </div>
         <div className={styles.progress_tabs}>
-          <div className={styles.left_button}>Left Button</div>
-          <div className={styles.tab1}>Tab 1</div>
-          <div className={styles.tab2}>Tab 2</div>
-          <div className={styles.tab3}>Tab 3</div>
-          <div className={styles.right_button}>Right Button</div>
+          <div className={styles.left_button}>
+            <div className={styles.button} />
+          </div>
+          <div className={styles.tab_wrapper_one}>
+            <div className={styles.tab}>Tab 1</div>
+          </div>
+          <div className={styles.tab_wrapper_two}>
+            <div className={styles.tab}>Tab 2</div>
+          </div>
+          <div className={styles.tab_wrapper_three}>
+            <div className={styles.tab_whats_next}>Whats next?</div>
+          </div>
+          <div className={styles.right_button}>
+            <div className={styles.button} />
+          </div>
         </div>
       </div>
     </div>
