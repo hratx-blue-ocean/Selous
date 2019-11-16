c434986eebfa2540e31db12d43a7716dd8f9863b
Devin George <devin.k.george@gmail.com>
2019-11-16 13:45:27 -0600

    Update actions

--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -1,5 +1,17 @@
-export default function toggleAction() {
-  return {
-    type: 'TOGGLE',
-  };
-}
+const toggleAction = () => ({ type: 'TOGGLE' });
+
+const addGoalAction = () => ({ type: 'ADD_GOAL' });
+
+const incrementGoalAction = () => ({ type: 'INCREMENT_GOAL' });
+
+const decrementGoalAction = () => ({ type: 'DECREMENT_GOAL' });
+
+const getGoalsAction = () => ({ type: 'GET_GOALS' });
+
+export default {
+  addGoalAction,
+  toggleAction,
+  incrementGoalAction,
+  decrementGoalAction,
+  getGoalsAction,
+};
