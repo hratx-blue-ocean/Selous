a5eae9b61db32c3089929a4db2b976b7757b7449
Devin George <devin.k.george@gmail.com>
2019-11-16 13:45:01 -0600

    Update reducers

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -2,9 +2,15 @@ import { combineReducers } from 'redux';
 
 // Reducers
 import toggleTestReducer from './toggleTestReducer.js';
+import addGoalReducer from './addGoalReducer.js';
+import getGoalsReducer from './getGoalsReducer.js';
+// import incrementGoalReducer from './incrementGoalReducer.js';
+// import decrementGoalReducer from './decrementGoalReducer.js';
 
 const rootReducer = combineReducers({
   toggle: toggleTestReducer,
+  addGoal: addGoalReducer,
+  currentGoals: getGoalsReducer,
 });
 
 export default rootReducer;
