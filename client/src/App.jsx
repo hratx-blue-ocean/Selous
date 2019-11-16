e6179503e098f6ec788b3488c018b9d89337e02f
Tyler Stanczak <tyler@Tylers-MacBook-Pro.local>
2019-11-16 10:44:50 -0600

    fix eslinter causing errors on headerbar and goals

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import fetch from 'node-fetch';
-import Goals from './components/goals.jsx';
+import React from 'react';
 
 // Imported for render testing
+import Goals from './components/goals.jsx';
 import DetailsPage from './components/DetailsPage/DetailsPage.jsx';
-import Headerbar from './components/headerbar/Headerbar.jsx'
+import Headerbar from './components/headerbar/Headerbar.jsx';
+
 export default function App() {
   return (
     <>
-      <Headerbar/>
-      <h1>Welcome to Blue Ocean!</h1>
-      <ul>
-        {result.map((creature, index) => (
-          <li key={index}>{creature}</li>
-        ))}
-      </ul>
+      <Headerbar />
       <Goals />
       <DetailsPage />
     </>
