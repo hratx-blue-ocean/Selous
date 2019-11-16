302831b1219b569210d624a62da41382c4ecf89e
Keenan Johns <keenanjj13@protonmail.com>
2019-11-16 11:52:17 -0600

    merge commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import './App.css';
-// import Goals from './components/goals.jsx';
-<<<<<<< HEAD
-
-=======
->>>>>>> 7c7c42e2a86e0cae499b1c06816787f82156949d
 import DetailsPage from './components/DetailsPage/DetailsPage.jsx';
 import Headerbar from './components/headerbar/Headerbar.jsx';
+// import Goals from './components/goals.jsx';
 
 export default function App() {
   return (
