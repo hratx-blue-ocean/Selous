c079abb93a9226ca1e78d346c8570afcf974e949
Juan Galeazzi <jgaleazzi@gmail.com>
2019-11-15 19:59:07 -0600

    TODO implementing state with redux

--- client/src/components/dashboard/dashModal.jsx
+++ client/src/components/dashboard/dashModal.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './dashboard.css';
 
 const DashModal = () => (
-  <div className={styles['dash-modal']}>
+  <div id="1dash-modal" className={styles['dash-step-modal']}>
     <div className={styles['dash-modal-triangle']}> </div>
     <div className={styles['dash-modal-elipse']}>
       Lorem Ipsum
--- client/src/components/dashboard/dashcolortile.jsx
+++ client/src/components/dashboard/dashcolortile.jsx
@@ -5,7 +5,19 @@ const DashColorTile = () => (
   <div
     className={styles['dash-step-tile']}
     onMouseEnter={(e) => {
-      console.log(e.target.getBoundingClientRect());
+      const { bottom, width, left } = e.target.getBoundingClientRect();
+      const modalWidth = document.getElementById('1dash-modal').clientWidth;
+      console.log(modalWidth, bottom, width, left);
+      // setState({
+      //  leftDSModalOffset: left + (width / 2) - (modalWidth / 2),
+      //  bottomDSModalOffset: bottom + 5,
+      //  DSmodalHidden = false,
+      // });
+    }}
+    onMouseLeave={() => {
+      // setState({
+      //   DSModalHidden = true;
+      // })
     }}
   />
 );
