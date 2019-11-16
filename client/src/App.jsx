7ba59f1fbb8fdd2d411e0c71065ff3677560a1b2
JoelArmendariz <aztemba@gmail.com>
2019-11-16 12:19:36 -0600

    commit to pull

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import fetch from 'node-fetch';
+import Goals from './components/goals.jsx';
 
 // Imported for render testing
-import Headerbar from './components/headerbar/Headerbar.jsx';
-
+import DetailsPage from './components/DetailsPage/DetailsPage.jsx';
+import Headerbar from './components/headerbar/Headerbar.jsx'
 export default function App() {
   return (
     <>
-      <Headerbar />
+      <Headerbar/>
+      <h1>Welcome to Blue Ocean!</h1>
+      <ul>
+        {result.map((creature, index) => (
+          <li key={index}>{creature}</li>
+        ))}
+      </ul>
+      <Goals />
+      <DetailsPage />
     </>
   );
 }
