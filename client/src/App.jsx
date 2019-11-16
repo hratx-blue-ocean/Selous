18631f7d295e15b3855b0f43dbfb608593b0dfd7
Keenan Johns <keenanjj13@protonmail.com>
2019-11-16 11:00:34 -0600

    Update App.jsx
    Co-Authored-By: Joel Armendariz <joelarmendariz@users.noreply.github.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import fetch from 'node-fetch';
-import Goals from './components/goals.jsx';
+import React from 'react';
 
 // Imported for render testing
 import DetailsPage from './components/DetailsPage/DetailsPage.jsx';
-import Headerbar from './components/headerbar/Headerbar.jsx'
+import Headerbar from './components/headerbar/Headerbar.jsx';
+
 export default function App() {
   return (
     <>
-      <Headerbar/>
-      <h1>Welcome to Blue Ocean!</h1>
-      <ul>
-        {result.map((creature, index) => (
-          <li key={index}>{creature}</li>
-        ))}
-      </ul>
-      <Goals />
+      <Headerbar />
+      {/* <Goals /> */}
       <DetailsPage />
     </>
   );
