2239d5fd7a11ec32ad171537a19d8af74813d0d0
Keenan Johns <keenanjj13@protonmail.com>
2019-11-18 12:20:04 -0600

    Dev pull commit

--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export const toggleAction = () => ({ type: 'TOGGLE' });
 
 export const addGoalAction = () => ({ type: 'ADD_GOAL' });
@@ -8,17 +7,5 @@ export const incrementGoalAction = (id) => ({ type: 'INCREMENT_GOAL', id });
 export const decrementGoalAction = (id) => ({ type: 'DECREMENT_GOAL', id });
 
 export const getGoalsAction = () => ({ type: 'GET_GOALS' });
-=======
-export function toggleAction() {
-  return {
-    type: 'TOGGLE',
-  };
-}
 
-export function AddJobAction(job) {
-  return {
-    type: 'ADD_JOB',
-    job,
-  };
-}
->>>>>>> dashboard
+export const AddJobAction = (job) => ({ type: 'ADD_JOB', job });
