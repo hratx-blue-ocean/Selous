e71d8e7916a8f96ad8618c4fc22c10558a92007a
MarioMorales7x7 <mario.morales.texas@gmail.com>
2019-11-18 12:22:39 -0600

    fixed merge conflicts from others

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import Headerbar from './components/headerbar/Headerbar.jsx';
 
 
@@ -8,22 +7,5 @@ export default function App() {
     <div>
       <Headerbar />
     </div>
-=======
-import Goals from './components/goals.jsx';
-
-// Imported for render testing
-import DetailsPage from './components/DetailsPage/DetailsPage.jsx';
-import Headerbar from './components/headerbar/Headerbar.jsx';
-import Dashboard from './components/dashboard/dashboard.jsx';
-
-export default function App() {
-  return (
-    <>
-      <Headerbar />
-      <Dashboard />
-      <Goals />
-      <DetailsPage />
-    </>
->>>>>>> dashboard
   );
 }
