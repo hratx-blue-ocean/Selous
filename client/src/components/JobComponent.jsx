69f7ac75cdc6a5b99c53404939a6533d0f81e7fd
Rob Peschke <50122770+RSP531@users.noreply.github.com>
2019-11-18 10:41:44 -0600

    Add files via upload

---
+++ client/src/components/JobComponent.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  Avatar,
+  Card,
+  Button,
+  Typography,
+  Grid,
+} from '@material-ui/core/';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import { combineReducers } from 'redux';
+
+const useStyles = makeStyles({
+  root: {
+    margin: 10,
+    borderWidth: 1,
+    borderColor: 'purple',
+    borderStyle: 'solid',
+    boxShadow: 3,
+    '&:hover': {
+      background: 'purple',
+    },
+    textTransform: 'none',
+    textAlign: 'left',
+  },
+  logo: {
+    margin: 10,
+
+  },
+  search_daysAgo: {
+    margin: 10,
+    alignItems: 'flex-end',
+  },
+  mainContainer: {
+    width: '100%',
+  },
+  text: {
+    width: '800px',
+  },
+});
+
+export default function PaperSheet() {
+  const classes = useStyles();
+
+  return (
+    <Button>
+      <Card className={classes.root} onClick={() => { console.log('joel sucks'); }}>
+        <Grid container item wrap="wrap" spacing={2} className={classes.mainContainer}>
+          <Grid item alignItems="center">
+            <Avatar className={classes.logo}>
+              <AssignmentIcon />
+            </Avatar>
+          </Grid>
+          <Grid container item xs direction="column" textAlign="left" className={classes.text}>
+            <Typography variant="h5" component="search_Position">
+              Senior Software Development Engineer
+            </Typography>
+            <Typography component="search_company">
+              Whole Foods Market
+            </Typography>
+            <Typography component="search_location">
+              Austin, TX
+            </Typography>
+          </Grid>
+          <Grid container item xs={3} display="flex" alignItems="flex-end">
+            <Typography component="search_daysAgo">
+              30+ days ago
+            </Typography>
+          </Grid>
+        </Grid>
+      </Card>
+    </Button>
+  );
+}
