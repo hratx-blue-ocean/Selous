cee3ff923c8efd7cf8e811bbbaea9853deaa6166
Keenan Johns <keenanjj13@protonmail.com>
2019-11-18 12:42:00 -0600

    Dev pull commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,8 @@
 import React from 'react';
-<<<<<<< HEAD
 import Headerbar from './components/headerbar/Headerbar.jsx';
-
-
-export default function App() {
-  return (
-    <div>
-      <Headerbar />
-    </div>
-=======
-import Goals from './components/goals.jsx';
-
-// Imported for render testing
 import DetailsPage from './components/DetailsPage/DetailsPage.jsx';
-import Headerbar from './components/headerbar/Headerbar.jsx';
 import Dashboard from './components/dashboard/dashboard.jsx';
+import Goals from './components/goals.jsx';
 
 export default function App() {
   return (
@@ -24,6 +12,5 @@ export default function App() {
       <Goals />
       <DetailsPage />
     </>
->>>>>>> dashboard
   );
 }
--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -2,21 +2,15 @@ import { combineReducers } from 'redux';
 
 // Reducers
 import toggleTestReducer from './toggleTestReducer.js';
-<<<<<<< HEAD
 import addGoalReducer from './addGoalReducer.js';
 import goalsHandlerReducer from './goalsHandlerReducer.js';
+import userJobsReducer from './userJobsReducer.js';
 
 const rootReducer = combineReducers({
   toggle: toggleTestReducer,
   addGoal: addGoalReducer,
   currentGoals: goalsHandlerReducer,
-=======
-import userJobsReducer from './userJobsReducer.js';
-
-const rootReducer = combineReducers({
-  toggle: toggleTestReducer,
   userJobs: userJobsReducer,
->>>>>>> dashboard
 });
 
 export default rootReducer;
