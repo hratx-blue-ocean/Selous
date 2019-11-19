f62b739ebb4b200fb26b0633a3df2ed20ef46800
Keenan Johns <keenanjj13@protonmail.com>
2019-11-19 16:59:25 -0600

    Revert "Broken commit" Going back a commit This reverts commit a935adde44a0a819ddefe6c4eb5b1f0a61e4bdfa.

--- client/src/components/DetailsPage/ProgressTabs/TabThree/TabThree.jsx
+++ client/src/components/DetailsPage/ProgressTabs/TabThree/TabThree.jsx
@@ -11,42 +11,34 @@ import WhatsNext from '../../../Modals/WhatsNext.jsx';
 const stylesArr = ['card_one', 'card_two', 'card_three', 'card_four', 'card_five', 'card_six', 'card_seven', 'card_eight'];
 const cardDepth = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'];
 
-const Tab = ({ tab, companyTabs, dispatch }) => {
-  // const handleOnClick = () => {
-  //   const companyTabsCopy = companyTabs.slice();
-  //   companyTabsCopy[companyTabsCopy.indexOf(tab)].completed = !companyTabsCopy[companyTabsCopy.indexOf(tab)].completed;
-  //   dispatch(setTabsCompanyTabsTEST(companyTabsCopy));
-  // };
-
-  return (
-    <>
-      <WhatsNext />
-      <div className={styles.tab_wrapper_three}>
-        <div className={styles.card_holder}>
-          {companyTabs.indexOf(tab) !== -1 ? (companyTabs.slice(companyTabs.indexOf(tab) + 1).length !== 0 ? companyTabs.slice(companyTabs.indexOf(tab) + 1).reduce((acc, cur, i, arr) => {
-            if (i === arr.length - 1) {
-              acc.push(styles[stylesArr[companyTabs.indexOf(cur)]], styles.whats_next_card);
-              return acc;
-            }
-            acc.push(styles[stylesArr[companyTabs.indexOf(cur)]]);
+const Tab = ({ tab, companyTabs, dispatch }) => (
+  <>
+    <WhatsNext />
+    <div className={styles.tab_wrapper_three}>
+      <div className={styles.card_holder}>
+        {companyTabs.indexOf(tab) !== -1 ? (companyTabs.slice(companyTabs.indexOf(tab) + 1).length !== 0 ? companyTabs.slice(companyTabs.indexOf(tab) + 1).reduce((acc, cur, i, arr) => {
+          if (i === arr.length - 1) {
+            acc.push(styles[stylesArr[companyTabs.indexOf(cur)]], styles.whats_next_card);
             return acc;
-          }, []).map((el, i) => {
-            return <span className={[el, styles[cardDepth[i]]].join(' ')} />;
-          }) : <span className={[styles.whats_next_card, styles[cardDepth[0]]].join(' ')} />) : ''}
-          <div className={tab ? (styles[tab.color ? tab.color : 'tab']) : null}>
-            <div className={styles.tab_header}>{tab ? tab.tabName : null}</div>
-            <div className={styles.tab_body}>{tab ? tab.tabBody : null}</div>
-            <div className={styles.tab_edit}>
-              <button type="button" onClick={() => dispatch(whatsNextAction())} className={styles.edit}>{tab ? tab.tabEditText : null}</button>
-            </div>
+          }
+          acc.push(styles[stylesArr[companyTabs.indexOf(cur)]]);
+          return acc;
+        }, []).map((el, i) => {
+          return <span className={[el, styles[cardDepth[i]]].join(' ')} />;
+        }) : <span className={[styles.whats_next_card, styles[cardDepth[0]]].join(' ')} />) : ''}
+        <div className={tab ? (styles[tab.color ? tab.color : 'tab']) : null}>
+          <div className={styles.tab_header}>{tab ? tab.tabName : null}</div>
+          <div className={styles.tab_body}>{tab ? tab.tabBody : null}</div>
+          <div className={styles.tab_edit}>
+            <button type="button" onClick={() => dispatch(whatsNextAction())} className={styles.edit}>{tab ? tab.tabEditText : null}</button>
           </div>
         </div>
-        {tab ? (tab.whatsNextTab ? '' : <div className={styles.check} onClick={() => ''} onKeyPress={() => ''} role="button" tabIndex={0}>{tab.completed ? (<svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.875 14.1486L4.22625 10.4999L2.98375 11.7336L7.875 16.6249L18.375 6.12486L17.1412 4.89111L7.875 14.1486Z" fill="black" /></svg>) : ''}</div>) : ''}
       </div>
-    </>
-  );
-};
+      {tab ? (tab.whatsNextTab ? '' : <div className={styles.check}>{tab.completed ? (<svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.875 14.1486L4.22625 10.4999L2.98375 11.7336L7.875 16.6249L18.375 6.12486L17.1412 4.89111L7.875 14.1486Z" fill="black" /></svg>) : ''}</div>) : ''}
+    </div>
+  </>
+);
 
-const mapStateToProps = (state) => ({ show: state.editModal });
+const mapStateToProps = (state) => ({ show: state.whatsNextModal });
 
 export default connect(mapStateToProps)(Tab);
