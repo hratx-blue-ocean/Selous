ea2d609a41c28dc2dbfeae44a370cc35c6d16d02
Keenan Johns <keenanjj13@protonmail.com>
2019-11-19 14:38:49 -0600

    Fixed right side tab stack color order

--- client/src/components/DetailsPage/ProgressTabs/TabThree/TabThree.jsx
+++ client/src/components/DetailsPage/ProgressTabs/TabThree/TabThree.jsx
@@ -16,15 +16,15 @@ const Tab = ({ tab, companyTabs, dispatch }) => (
     <WhatsNext />
     <div className={styles.tab_wrapper_three}>
       <div className={styles.card_holder}>
-        {companyTabs.indexOf(tab) !== -1 ? (companyTabs.slice(companyTabs.indexOf(tab) + 1).length !== 0 ? companyTabs.slice(companyTabs.indexOf(tab) + 1).reduce((acc, cur, i) => {
-          if (i === 0) {
-            acc.unshift(styles[stylesArr[companyTabs.indexOf(tab) + 1]], styles.whats_next_card);
+        {companyTabs.indexOf(tab) !== -1 ? (companyTabs.slice(companyTabs.indexOf(tab) + 1).length !== 0 ? companyTabs.slice(companyTabs.indexOf(tab) + 1).reduce((acc, cur, i, arr) => {
+          if (i === arr.length - 1) {
+            acc.push(styles[stylesArr[companyTabs.indexOf(cur)]], styles.whats_next_card);
             return acc;
           }
-          acc.unshift(styles[stylesArr[companyTabs.indexOf(tab) + 1]]);
+          acc.push(styles[stylesArr[companyTabs.indexOf(cur)]]);
           return acc;
         }, []).map((el, i) => {
-          return <span className={[styles[cardDepth[i]], el].join(' ')} />;
+          return <span className={[el, styles[cardDepth[i]]].join(' ')} />;
         }) : <span className={[styles.whats_next_card, styles[cardDepth[0]]].join(' ')} />) : ''}
         <div className={tab ? (styles[tab.color ? tab.color : 'tab']) : null}>
           <div className={styles.tab_header}>{tab ? tab.tabName : null}</div>
