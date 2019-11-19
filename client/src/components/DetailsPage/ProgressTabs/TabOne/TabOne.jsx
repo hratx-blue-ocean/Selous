65fb855cb5e421064dda6f2e57a02dfad24be487
Keenan Johns <keenanjj13@protonmail.com>
2019-11-19 12:26:13 -0600

    Minor CSS updates

--- client/src/components/DetailsPage/ProgressTabs/TabOne/TabOne.jsx
+++ client/src/components/DetailsPage/ProgressTabs/TabOne/TabOne.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable arrow-body-style */
+/* eslint-disable array-callback-return */
 /* eslint-disable max-len */
 import React from 'react';
 import { connect } from 'react-redux';
@@ -6,14 +8,19 @@ import EditDetailsModal from '../../../Modals/EditModal.jsx';
 import { editAction } from '../../../../redux/actions/actions.js';
 
 const stylesArr = ['card_one', 'card_two', 'card_three', 'card_four', 'card_five', 'card_six', 'card_seven', 'card_eight'];
-const cardDepth = ['one', 'two', 'three'];
+const cardDepth = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'];
 
 const Tab = ({ tab, companyTabs, dispatch }) => (
   <>
     <EditDetailsModal />
     <div className={styles.tab_wrapper_one}>
       <div className={styles.card_holder}>
-        {companyTabs.slice(0, companyTabs.indexOf(tab)).map((el, i) => (<span key={i} className={[styles[cardDepth[i]], styles[stylesArr[i]]].join(' ')} />))}
+        {companyTabs.slice(0, companyTabs.indexOf(tab)).reduce((acc, cur, i) => {
+          acc.unshift(styles[stylesArr[i]]);
+          return acc;
+        }, []).map((el, i) => {
+          return <span className={[styles[cardDepth[i]], el].join(' ')} />;
+        })}
         <div className={tab ? (styles[tab.color ? tab.color : 'tab']) : null}>
           <div className={styles.tab_header}>{tab ? tab.tabName : null}</div>
           <div className={styles.tab_body}>{tab ? tab.tabBody : null}</div>
--- client/src/components/DetailsPage/ProgressTabs/TabThree/TabThree.jsx
+++ client/src/components/DetailsPage/ProgressTabs/TabThree/TabThree.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable arrow-body-style */
 /* eslint-disable no-nested-ternary */
 /* eslint-disable array-callback-return */
 /* eslint-disable max-len */
@@ -8,24 +9,23 @@ import { whatsNextAction } from '../../../../redux/actions/actions.js';
 import WhatsNext from '../../../Modals/WhatsNext.jsx';
 
 const stylesArr = ['card_one', 'card_two', 'card_three', 'card_four', 'card_five', 'card_six', 'card_seven', 'card_eight'];
-const cardDepth = ['two', 'three'];
+const cardDepth = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight'];
 
 const Tab = ({ tab, companyTabs, dispatch }) => (
   <>
     <WhatsNext />
     <div className={styles.tab_wrapper_three}>
       <div className={styles.card_holder}>
-        {companyTabs.indexOf(tab) !== -1 ? (companyTabs.slice(companyTabs.indexOf(tab) + 1).length !== 0 ? companyTabs.slice(companyTabs.indexOf(tab) + 1).map((el, i) => {
+        {companyTabs.indexOf(tab) !== -1 ? (companyTabs.slice(companyTabs.indexOf(tab) + 1).length !== 0 ? companyTabs.slice(companyTabs.indexOf(tab) + 1).reduce((acc, cur, i) => {
           if (i === 0) {
-            return (
-              <>
-                <span className={styles.whats_next_card} />
-                <span className={[styles[cardDepth[i]], styles[stylesArr[companyTabs.indexOf(tab) + 1]]].join(' ')} />
-              </>
-            );
+            acc.unshift(styles[stylesArr[companyTabs.indexOf(tab) + 1]], styles.whats_next_card);
+            return acc;
           }
-          return <span className={[styles[cardDepth[i]], styles[companyTabs.indexOf(tab) + 1]].join(' ')} />;
-        }) : <span className={styles.whats_next_card} />) : ''}
+          acc.unshift(styles[stylesArr[companyTabs.indexOf(tab) + 1]]);
+          return acc;
+        }, []).map((el, i) => {
+          return <span className={[styles[cardDepth[i]], el].join(' ')} />;
+        }) : <span className={[styles.whats_next_card, styles[cardDepth[0]]].join(' ')} />) : ''}
         <div className={tab ? (styles[tab.color ? tab.color : 'tab']) : null}>
           <div className={styles.tab_header}>{tab ? tab.tabName : null}</div>
           <div className={styles.tab_body}>{tab ? tab.tabBody : null}</div>
