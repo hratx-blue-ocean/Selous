164e548c5155dd8a554d31f1d5c37d5f685ec874
Keenan Johns <keenanjj13@protonmail.com>
2019-11-20 14:26:03 -0600

    Css styling tweaks

---
+++ client/src/redux/reducers/companyNotesTESTReducer.js
@@ -0,0 +1,12 @@
+const defaultData = {
+  companyName: '',
+  companyDetails: '',
+  notes: '',
+};
+
+export default function companyNotesTESTReducer(state = defaultData, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
--- client/src/redux/reducers/companyTabsReducerTEST.js
+++ client/src/redux/reducers/companyTabsReducerTEST.js
@@ -1,4 +1,4 @@
-// const test1 = [];
+const test1 = [];
 
 // const test2 = [
 //   {
@@ -119,50 +119,50 @@
 //   },
 // ];
 
-const test7 = [
-  {
-    whatsNextTab: false,
-    tabName: 'Title Page',
-    tabBody: '',
-    tabEditText: 'Edit',
-    completed: true,
-  },
-  {
-    whatsNextTab: false,
-    tabName: 'Resume',
-    tabBody: '',
-    tabEditText: 'Edit',
-    completed: true,
-  },
-  {
-    whatsNextTab: false,
-    tabName: 'Phone Interview',
-    tabBody: '',
-    tabEditText: 'Edit',
-    completed: true,
-  },
-  {
-    whatsNextTab: false,
-    tabName: 'Onsite Interview',
-    tabBody: '',
-    tabEditText: 'Edit',
-    completed: true,
-  },
-  {
-    whatsNextTab: false,
-    tabName: 'Round 2 Interview',
-    tabBody: '',
-    tabEditText: 'Edit',
-  },
-  {
-    whatsNextTab: false,
-    tabName: 'Offer',
-    tabBody: '',
-    tabEditText: 'Edit',
-  }
-];
+// const test7 = [
+//   {
+//     whatsNextTab: false,
+//     tabName: 'Title Page',
+//     tabBody: '',
+//     tabEditText: 'Edit',
+//     completed: true,
+//   },
+//   {
+//     whatsNextTab: false,
+//     tabName: 'Resume',
+//     tabBody: '',
+//     tabEditText: 'Edit',
+//     completed: true,
+//   },
+//   {
+//     whatsNextTab: false,
+//     tabName: 'Phone Interview',
+//     tabBody: '',
+//     tabEditText: 'Edit',
+//     completed: true,
+//   },
+//   {
+//     whatsNextTab: false,
+//     tabName: 'Onsite Interview',
+//     tabBody: '',
+//     tabEditText: 'Edit',
+//     completed: true,
+//   },
+//   {
+//     whatsNextTab: false,
+//     tabName: 'Round 2 Interview',
+//     tabBody: '',
+//     tabEditText: 'Edit',
+//   },
+//   {
+//     whatsNextTab: false,
+//     tabName: 'Offer',
+//     tabBody: '',
+//     tabEditText: 'Edit',
+//   },
+// ];
 
-export default function companyTabsReducerTEST(state = test7, action) {
+export default function companyTabsReducerTEST(state = test1, action) {
   switch (action.type) {
     case 'GET_COMPANY_TABS':
       return state;
