56ec36393c4ce8ccbf2c442c55054bfcbd3f8577
Keenan Johns <keenanjj13@protonmail.com>
2019-11-20 12:12:27 -0600

    Tab edit buttons hidden if no info is passed to them

--- client/src/components/DetailsPage/ProgressTabs/TabTwo/TabTwo.jsx
+++ client/src/components/DetailsPage/ProgressTabs/TabTwo/TabTwo.jsx
@@ -22,9 +22,11 @@ const Tab = ({ tab, companyTabsTEST, dispatch }) => {
         <div className={[tab ? (styles[tab.color ? tab.color : 'tab']) : styles.border_gray, styles.tab].join(' ')}>
           <div className={styles.tab_header}>{tab ? tab.tabName : null}</div>
           <div className={styles.tab_body}>{tab ? tab.tabBody : null}</div>
-          <div className={styles.tab_edit}>
-            <button type="button" onClick={() => dispatch(editAction())} className={styles.edit}>{tab ? tab.tabEditText : null}</button>
-          </div>
+          {tab ? (
+            <div className={styles.tab_edit}>
+              <button type="button" onClick={() => dispatch(editAction())} className={styles.edit}>{tab.tabEditText}</button>
+            </div>
+          ) : ''}
         </div>
         {tab ? (tab.whatsNextTab ? '' : <button type="button" className={styles.check} onClick={handleOnClick}>{tab.completed ? (<svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.875 14.1486L4.22625 10.4999L2.98375 11.7336L7.875 16.6249L18.375 6.12486L17.1412 4.89111L7.875 14.1486Z" fill="black" /></svg>) : ''}</button>) : ''}
       </div>
