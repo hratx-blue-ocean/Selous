3e876b766b401655b1fd7bd85e4d6749641018ac
Devin George <devin.k.george@gmail.com>
2019-11-20 18:19:25 -0600

    Debug error in db file

--- database/db.js
+++ database/db.js
@@ -158,14 +158,15 @@ const changeProgress = (userId, jobId, progId, completed, callback) => {
 
 const editProgress = (userId, jobId, progressId, progressData, callback) => {
   User.findOne({ _id: userId }).then((user) => {
-    user.userJobs[jobId].progressArray[progressId] = progressData;
+    const thisUser = user;
+    thisUser.userJobs[jobId].progressArray[progressId] = progressData;
     user.save((data) => {
       callback(null, data);
     }).catch((err) => {
       callback(err, null);
     });
   });
-
+};
 // Goal Schema
 // const goal = {
 //   goaldId: 3,
@@ -227,6 +228,4 @@ const editProgress = (userId, jobId, progressId, progressData, callback) => {
 
 // All exported functions work!
 
-module.exports = {
-  addJob, addGoal, addJobProgress, validateLogin, validateSignup, changeProgress, editProgress
-};
+module.exports = { addJob, addGoal, addJobProgress, validateLogin, validateSignup, changeProgress, editProgress };
