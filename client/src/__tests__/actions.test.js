fb8506a4bb74cad7c405632fd2fb85de84be06e2
Devin George <devin.k.george@gmail.com>
2019-11-21 14:59:54 -0600

    Add layout for redux test

---
+++ client/src/__tests__/actions.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as actions from '../redux/actions/actions.js';
+// import * as types from '../../constants/ActionTypes'
+
+describe('actions', () => {
+  it('should create an action to add a todo', () => {
+    const text = 'Finish docs'
+    const expectedAction = {
+      type: types.ADD_TODO,
+      text
+    }
+    expect(actions.addTodo(text)).toEqual(expectedAction)
+  })
+})
