37f414b1751933a79efa66d7dbd7b0ea772b79b1
Devin George <devin.k.george@gmail.com>
2019-11-21 18:49:29 -0600

    Add more tests for actions reducers goals

---
+++ client/src/__tests__/Goals.test.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+// import React from 'react';
+// import { render, cleanup } from '@testing-library/react';
+// import Goals from '../components/Goals/Goals.jsx';
+
+test('true is true', () => {
+  expect(true).toBe(true);
+});
+
+// describe('Login', () => {
+//   afterEach(cleanup);
+
+//   it('should render without crashing', () => {
+//     const { container } = render(
+//       <Goals />,
+//     );
+//     expect(container).toBeTruthy();
+//   });
+// });
